51e038daa081ac453976de0384f7aa04c8d03bf1
William T. Nelson <35801+wtn@users.noreply.github.com>
2020-01-31 13:41:43 -0600

    add tests demonstrating behavior

--- config/initializers/tenant_databases.rb
+++ config/initializers/tenant_databases.rb
@@ -1,13 +1,22 @@
 module TenantDatabases
-  app_name = Rails.application.class.module_parent.name.underscore
+  APP_NAME = Rails.application.class.module_parent.name.underscore
   INITIAL_DB_COUNT = ActiveRecord::Base.configurations.configs_for.size
-  TENANT_SPEC_NAMES = [10101, 53791, 24680].map {|id| "#{app_name}_#{id}"}
+  TENANT_SPEC_NAMES = [10101, 53791, 24680].map {|id| "#{APP_NAME}_#{id}"}
+  DESIRED_DB_COUNT = INITIAL_DB_COUNT + TENANT_SPEC_NAMES.size
 
-  TENANT_SPEC_NAMES.each do |tenant_spec_name|
-    tenant_config = ActiveRecord::Base.configurations.configs_for(spec_name: 'primary', env_name: Rails.env).config.deep_dup.tap do |h|
-      h['database'] = h['database'].sub(app_name, tenant_spec_name)
+  class << self
+    def inject_tenant_db_configs
+      configurations = ActiveRecord::Base.configurations.configurations
+      warn('already injected') if DESIRED_DB_COUNT == ActiveRecord::Base.configurations.configs_for.size
+      TENANT_SPEC_NAMES.each do |tenant_spec_name|
+        tenant_config = ActiveRecord::Base.configurations.configs_for(spec_name: 'primary', env_name: Rails.env).config.deep_dup.tap do |h|
+          h['database'] = h['database'].sub(APP_NAME, tenant_spec_name)
+        end
+        configurations << ActiveRecord::DatabaseConfigurations::HashConfig.new(Rails.env, tenant_spec_name, tenant_config)
+      end
+      ActiveRecord::Base.configurations = ActiveRecord::DatabaseConfigurations.new configurations
     end
-    ActiveRecord::Base.configurations.configurations \
-      << ActiveRecord::DatabaseConfigurations::HashConfig.new(Rails.env, tenant_spec_name, tenant_config)
   end
 end
+
+TenantDatabases.inject_tenant_db_configs
--- test/db_setup_test.rb
+++ test/db_setup_test.rb
@@ -1,9 +1,38 @@
 require 'test_helper'
 
 class DBSetupTest < ActiveSupport::TestCase
-  include TenantDatabases
+  self.test_order = :sorted
 
-  test 'injected configurations loaded' do
-    assert_equal INITIAL_DB_COUNT + TENANT_SPEC_NAMES.size, ActiveRecord::Base.configurations.configs_for.size
+  test '1 extra configs set by initializer' do
+    assert_equal TenantDatabases::DESIRED_DB_COUNT, configs_count
+  end
+
+  test '2 rake db tasks reset configs' do
+    lost_configs_difference = -1 * TenantDatabases::TENANT_SPEC_NAMES.size
+
+    assert_difference('configs_count', lost_configs_difference) do
+      silence_stream($stdout) { Rake::Task['db:version'].invoke }
+    end
+  end
+
+  test '3 restore extra configs' do
+    TenantDatabases.inject_tenant_db_configs
+    assert_equal TenantDatabases::DESIRED_DB_COUNT, configs_count
+  end
+
+  private
+
+  def configs_count
+    ActiveRecord::Base.configurations.configs_for.size
+  end
+
+  def silence_stream(stream)
+    old_stream = stream.dup
+    stream.reopen(RbConfig::CONFIG['host_os'] =~ /mswin|mingw/ ? 'NUL:' : '/dev/null')
+    stream.sync = true
+    yield
+  ensure
+    stream.reopen(old_stream)
+    old_stream.close
   end
 end
